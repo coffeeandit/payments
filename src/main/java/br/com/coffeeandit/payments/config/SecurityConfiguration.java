2370c3791724320b05c5818b18bc15d38a79131b
Coffee and IT <channelcoffeeandit@gmail.com>
2023-08-07 17:33:27 -0300

    Especification

--- src/main/java/br/com/coffeeandit/payments/config/SecurityConfiguration.java
+++ src/main/java/br/com/coffeeandit/payments/config/SecurityConfiguration.java
@@ -20,7 +20,7 @@ public class SecurityConfiguration {
 
         return httpSecurity
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/actuator/**", "/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/actuator/**", "/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/v3/api-docs").permitAll()
                         .requestMatchers("/pagamento/**").hasAnyAuthority(SCOPE_COFFEE_AND_IT_ROLE)
                         .anyRequest()
                         .authenticated()
