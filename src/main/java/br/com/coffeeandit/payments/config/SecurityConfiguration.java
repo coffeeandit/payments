e47d4815097a640759bc3bfa5b1b7a51f1003996
Coffee and IT <channelcoffeeandit@gmail.com>
2023-08-07 11:49:05 -0300

    JWT

--- src/main/java/br/com/coffeeandit/payments/config/SecurityConfiguration.java
+++ src/main/java/br/com/coffeeandit/payments/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,11 +21,11 @@ public class SecurityConfiguration {
                         .anyRequest()
                         .authenticated()
                 )
-                .csrf(csrf -> csrf.disable())
-                .formLogin(form -> form.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-                .httpBasic(http -> http.disable())
+                .oauth2ResourceServer(Customizer.withDefaults())
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .build();
     }
 
