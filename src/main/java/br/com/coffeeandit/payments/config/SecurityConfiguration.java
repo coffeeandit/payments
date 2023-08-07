f11956c5fca4937b2ae661b1b92cb74a8455db7f
Coffee and IT <channelcoffeeandit@gmail.com>
2023-08-07 19:39:07 -0300

    Security

--- src/main/java/br/com/coffeeandit/payments/config/SecurityConfiguration.java
+++ src/main/java/br/com/coffeeandit/payments/config/SecurityConfiguration.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
